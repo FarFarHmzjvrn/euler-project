c1908ba09abad0101cf65fafd842515340bbbecf
Farnaz <frnz_hmzjvrn@outlook.com>
2023-01-11 11:15:47 -0800

    add def triangle function

---
+++ questions/45.py
@@ -0,0 +1,10 @@
+def Triangle(resault):
+    """The check that asked a number,
+    which number is in the sequence of triangular numbers
+    """
+    n = 1
+    while True:
+        if (n)*(n+1)*(0.5) == resault:
+            return n
+        n += 1
+
