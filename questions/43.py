e190c112fd49a3ac3cba1b2f965338d54cf6bfbe
Farnaz <frnz_hmzjvrn@outlook.com>
2023-01-13 10:52:00 -0800

    update

--- questions/43.py
+++ questions/43.py
@@ -2,3 +2,11 @@
 # create a function to get mod
 def subdiv(num, n):
     return num % n == 0
+
+#Using permutation for different combinations of these numbers
+# 0,1,2,3,4,5,6,7,8,9
+
+
+
+
+#variable to store the value of sum
