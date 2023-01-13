d90116d87941c3d56c965bfc45a1582387e8c741
Farnaz <frnz_hmzjvrn@outlook.com>
2023-01-13 10:59:39 -0800

    update

--- questions/43.py
+++ questions/43.py
@@ -11,5 +11,18 @@ x = permutations('0123456789')
 
 
 #variable to store the value of sum
-
+total = 0
 #for loop to loop through permutations
+for num in x:
+    if (subdiv(int(''.join(num[7:10])), 17)and
+       subdiv(int(''.join(num[6:9])), 13)and
+       subdiv(int(''.join(num[5:8])), 11)and
+       subdiv(int(''.join(num[4:7])), 7)and
+       subdiv(int(''.join(num[3:6])), 5)and
+       subdiv(int(''.join(num[2:5])), 3)and
+       subdiv(int(''.join(num[1:4])), 2)):
+        total += int(''.join(num))
+
+
+
+total
